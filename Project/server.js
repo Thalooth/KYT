a47728122117c78ea9fcddde9636a290d8c13a19
ShahalKP <info4shahal@gmail.com>
2019-11-21 23:47:50 +0530

    Setup server

--- Project/server.js
+++ Project/server.js
@@ -1,27 +1,42 @@
 const fs = require('fs');
+const express = require('express');
 const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
-const visualRecognition = new VisualRecognitionV3({
-  version: '2018-03-19',
-  authenticator: new IamAuthenticator({
-    apikey: 'NJI6LS5Mc5GFMxRigm71P1ciRG2sE3196iW96Cz1_2zP',
-  }),
-  url: 'https://gateway.watsonplatform.net/visual-recognition/api',
+// Starting our app.
+const app = express();
+
+// Starting our server.
+app.listen(3000, () => {
+    console.log('Listening..');
 });
 
-
-const classifyParams = {
-    imagesFile: fs.createReadStream('./Y246.JPG'),
-    owners: ['me'],
-    threshold: 0.6,
-};
-
-visualRecognition.classify(classifyParams)
-.then(response => {
-    const classifiedImages = response.result;
-    console.log(JSON.stringify(classifiedImages, null, 2));
+app.get('/upload', function(req, res){
+    const visualRecognition = new VisualRecognitionV3({
+        version: '2018-03-19',
+        authenticator: new IamAuthenticator({
+          apikey: 'NJI6LS5Mc5GFMxRigm71P1ciRG2sE3196iW96Cz1_2zP',
+        }),
+        url: 'https://gateway.watsonplatform.net/visual-recognition/api',
+      });
+      
+      
+      const classifyParams = {
+          imagesFile: fs.createReadStream('./Y246.JPG'),
+          owners: ['me'],
+          threshold: 0.6,
+      };
+      
+      visualRecognition.classify(classifyParams)
+      .then(response => {
+          const classifiedImages = response.result;
+          let classes = classifiedImages.images[0].classifiers[0].classes
+          console.log(classes);
+          //console.log(JSON.stringify(classifiedImages, null, 2));
+      })
+      .catch(err => {
+          console.log('error:', err);
+      });
 })
-.catch(err => {
-    console.log('error:', err);
-});
+
+
